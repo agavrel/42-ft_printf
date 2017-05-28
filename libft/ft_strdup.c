d254ebe674f732a6ed6f27cf210c2105b53b75f9
Antonin GAVREL <angavrel@e2r12p14.42.fr>
2017-05-28 08:13:44 +0200

    strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/28 07:47:35 by angavrel          #+#    #+#             */
+/*   Updated: 2017/05/28 08:10:49 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s;
+	int		len;
+
+	len = 0;
+	while (s1[len])
+		++len;
+	if (!(s = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s[len] = '\0';
+	while (len--)
+		s[len] = s1[len];
+	return (s);
+}
